643cc215f1c678aad96b7781581986294a3b8783
duckgii <shinyonggi@naver.com>
2024-05-05 19:25:08 +0900

    Style : for norm

---
+++ philo_bonus/init_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duckgi <duckgi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 14:27:16 by yeoshin           #+#    #+#             */
+/*   Updated: 2024/05/05 19:14:42 by duckgi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher_bonus.h"
+
+static int		*fork_init(int size);
+static t_philo	*philosopher_init(int ac, char *av[]);
+
+t_philo	**init_philo(int ac, char *av[])
+{
+	t_philo	**ret;
+	t_philo	*philosopher;
+	int		*right_fork;
+	int		*left_fork;
+	int		idx;
+
+	idx = 0;
+	ret = ft_malloc(sizeof(t_philo *) * ft_atoi(av[1]));
+	right_fork = fork_init(ft_atoi(av[1]));
+	left_fork = NULL;
+	while (idx < ft_atoi(av[1]))
+	{
+		philosopher = philosopher_init(ac, av);
+		philosopher->left_fork = left_fork;
+		philosopher->right_fork = &(right_fork[idx]);
+		philosopher->philo_num = idx + 1;
+		philosopher->start_time = 0;
+		philosopher->live = ALIVE;
+		left_fork = &(right_fork[idx]);
+		ret[idx] = philosopher;
+		idx++;
+	}
+	ret[0]->left_fork = left_fork;
+	return (ret);
+}
+
+pthread_mutex_t	*mutex_init(void)
+{
+	pthread_mutex_t	*mutex;
+
+	mutex = ft_malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(mutex, NULL);
+	return (mutex);
+}
+
+static int	*fork_init(int size)
+{
+	int	idx;
+	int	*forks;
+
+	idx = 0;
+	forks = ft_malloc(sizeof(int) * size);
+	while (idx < size)
+	{
+		forks[idx] = UNUSED;
+		idx++;
+	}
+	return (forks);
+}
+
+static t_philo	*philosopher_init(int ac, char *av[])
+{
+	t_philo	*philosopher;
+	int		num;
+
+	num = UNUSED;
+	philosopher = ft_malloc(sizeof(t_philo));
+	philosopher->die_time = ft_atoi(av[2]);
+	philosopher->eat_time = ft_atoi(av[3]);
+	philosopher->sleep_time = ft_atoi(av[4]);
+	if (ac == 5)
+		philosopher->must_eat_count = -1;
+	else if (ac == 6)
+		philosopher->must_eat_count = ft_atoi(av[5]);
+	philosopher->mutex = mutex_init();
+	philosopher->right_fork = &num;
+	philosopher->left_fork = NULL;
+	return (philosopher);
+}
+
+t_monitor	*init_monitor(t_philo **philo, int philo_count)
+{
+	t_monitor	*monitor;
+
+	monitor = ft_malloc(sizeof(t_monitor));
+	monitor->philo = philo;
+	monitor->philo_count = philo_count;
+	return (monitor);
+}
---
+++ philo_bonus/philosopher_bonus.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duckgi <duckgi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 14:53:20 by yeoshin           #+#    #+#             */
+/*   Updated: 2024/05/05 19:15:28 by duckgi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_BONUS_H
+# define PHILOSOPHER_BONUS_H
+
+# include <stdio.h>
+# include "./libft/libft.h"
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_philo{
+	int				die_time;
+	int				eat_time;
+	int				sleep_time;
+	long			start_time;
+	long			start_starve;
+	int				must_eat_count;
+	int				eat_count;
+	int				*left_fork;
+	int				*right_fork;
+	int				philo_num;
+	int				live;
+	pthread_mutex_t	*mutex;
+}	t_philo;
+
+typedef struct s_monitor{
+	t_philo	**philo;
+	int		philo_count;
+}	t_monitor;
+
+enum {
+	USE = 1,
+	UNUSED,
+	FIRST_EAT,
+	SECOND_EAT,
+	ALIVE,
+	DIE,
+};
+
+enum {
+	FALSE = 0,
+	TRUE,
+};
+
+t_philo			**init_philo(int ac, char *av[]);
+void			*thread_function(void *arg);
+pthread_mutex_t	*mutex_init(void);
+long			get_time(t_philo *philo);
+void			start_philo(t_philo *philo);
+void			mutex_lock_and_think(t_philo *philo);
+int				get_fork(int eat_time, t_philo *philo);
+void			end_eat(t_philo *philo);
+void			*monitoring_thread(void *arg);
+t_monitor		*init_monitor(t_philo **philo, int philo_count);
+int				check_live(t_philo *philo);
+void			check_and_change_live(t_philo *philo);
+void			change_fork_status(t_philo *philo, int *fork, int status);
+int				check_fork(t_philo *philo, int *fork);
+void			up_eat_count(t_philo *philo);
+int				check_eat_count(t_philo *philo);
+
+#endif
