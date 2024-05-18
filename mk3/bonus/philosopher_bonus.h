583c00a81095a64514339457af955b0795f20252
Yeonggi Shin <yeoshin@c2r4s3.42seoul.kr>
2024-05-18 11:00:59 +0900

    Style : make bonus, mandatory directory

---
+++ mk3/bonus/philosopher_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_bonus.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeoshin <yeoshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 10:59:01 by yeoshin           #+#    #+#             */
+/*   Updated: 2024/05/18 11:00:11 by yeoshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_BONUS_H
+# define PHILOSOPHER_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
